0c7a66e53f2ffcab7e0f639df6956e5b925dce2c
Scute0001 <s12t8811@gmail.com>
2020-09-29 16:49:34 +0800

    commit step2

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -99,9 +100,16 @@ class SleepTrackerFragment : Fragment() {
 
         // TODO (04) Create a new SleepNightAdapter variable,
         // and bind it to the RecyclerView’s Adapter.
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
 
         // TODO (05) Create an observer on sleepTrackerViewModel.nights that tells
         // the Adapter when there is new data.
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
 
         return binding.root
     }
